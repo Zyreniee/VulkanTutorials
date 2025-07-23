f19abc1c72f394efc867682fe62a02bf0f075907
Yusuf Gön <gunes4122yy@gmail.com>
2025-07-23 17:50:14 +0300

    0.25 TEST

--- lve_pipeline.cpp
+++ lve_pipeline.cpp
@@ -8,7 +8,8 @@ using namespace std;
 #include <stdexcept>
 namespace Lve2 {
 
-
+	//You should solve this problem tomorrow
+	//Problem : Error in debug at this and lve_pipeline.hpp
 
 }
 namespace lve {
