22979641037b98d3e8dc06c5080b87e860b01177
Yusuf Gön <gunes4122yy@gmail.com>
2025-07-25 23:50:55 +0300

    0.28 TEST

---
+++ main2.cpp
@@ -0,0 +1,8 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+using namespace std;
+int main() {
+	std::cout << "Hello the other tutorial (every finishes are always new starts)" << endl;
+	return 0;
+}
