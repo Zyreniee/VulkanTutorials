fb33cc9fd794baa3169211e08ed18913cfc8857c
Yusuf Gön <gunes4122yy@gmail.com>
2025-08-03 20:54:32 +0300

    0.32  TEST

---
+++ engine.cpp
@@ -0,0 +1,7 @@
+#include <GLFW/glfw3.h>
+#define GLFW_INCLUDE_VULKAN
+#include <vulkan/vulkan.hpp>
+using namespace std;
+int main() {
+	make_instance();
+ }
--- main2.cpp
+++ main2.cpp
@@ -1,7 +1,9 @@
-#include <GLFW/glfw3.h>
 #include <iostream>
-
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
 using namespace std;
+
 int main() {
 	std::cout << "Hello the other tutorial (every finishes are always new starts)" << endl;
 	return 0;
