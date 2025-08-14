a2931edf3e66e1976a2eb175d9c1e661be5f18eb
Yusuf Gön <gunes4122yy@gmail.com>
2025-08-14 16:48:54 +0300

    0.58 TEST

--- compile.bat
+++ compile.bat
@@ -1,3 +1,11 @@
-C:\VulkanSDK\1.4.313.2\Bin\glslc.exe shaders\simple_shader.vert -o shaders\simple_shader.vert.spv
-C:\VulkanSDK\1.4.313.2\Bin\glslc.exe shaders\simple_shader.frag -o shaders\simple_shader.frag.spv
+@echo off
+REM Script'in bulunduðu klasöre geç
+cd /d "%~dp0"
+
+REM Vertex shader'ý derle
+C:\VulkanSDK\1.4.313.2\Bin\glslc.exe Shaders\simple_shader.vert -o Shaders\simple_shader.vert.spv
+
+REM Fragment shader'ý derle
+C:\VulkanSDK\1.4.313.2\Bin\glslc.exe Shaders\simple_shader.frag -o Shaders\simple_shader.frag.spv
+
 pause
--- compile.sh
+++
@@ -1,2 +0,0 @@
-/usr/local/bin/glslc shader/simple_shader.vert -o shaders/simple_shader.vert.spv
-/usr/local/bin/glslc shader/simple_shader.frag -o shaders/simple_shader.frag.spv
